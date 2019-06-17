d504183fdf878d1fe53830f83002f086f5374a93
kevin <undyingmind@gmail.com>
2019-06-17 21:59:50 +0700

    Added option for createService

--- src/provider.js
+++ src/provider.js
@@ -10,26 +10,26 @@ export default class ServiceProvider {
   getService(name, options) {
     let Service = this._collection.services[name.toLowerCase()]
     if (!Service) return null
-    return this._createService(Service, options)
+    return this.createService(Service, options)
   }
 
   getRequiredService(name, options) {
     let Service = this._collection.services[name.toLowerCase()]
     if (!Service)
-      throw new Error(`Missing required service: ${name}`)
-    return this._createService(Service, options)
+      throw new Error(`Missing required service "${name}"`)
+    return this.createService(Service, options)
   }
 
-  _createService(Service) {
+  createService(Service, options) {
     let service, name = Service.service
     if (Service.isSingleton) {
       service = this._instances[name]
       if (!service) {
-        service = Service.isConcrete ? Service() : new Service(this)
+        service = Service.isConcrete ? Service() : new Service(this, options)
         this._instances[name] = service
       }
     } else {
-      service = new Service(this)
+      service = new Service(this, options)
     }
     return service
   }
